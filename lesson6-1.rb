f8c4c9f5b5004ec19f8e044183c38b4c1adcd85e
EC2 Default User <ec2-user@ip-172-31-8-55.ap-northeast-1.compute.internal>
2023-01-09 09:06:29 +0000

    [Finish] 2-6

---
+++ lesson6-1.rb
@@ -0,0 +1,7 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+num1 = gets.to_i
+num2 = gets.to_i
+puts "計算結果を出力します"
+puts "#{num1}*#{num2}=#{num1*num2}"
+puts "計算を終了します"
---
+++ lesson6-2.rb
@@ -0,0 +1,26 @@
+i = 1
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+steps = gets.to_i
+
+while i <= steps do
+
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  num1 = gets.to_i
+  num2 = gets.to_i
+  puts "a=#{num1}"
+  puts "b=#{num2}"
+  puts "計算結果を出力します"
+  puts "#{num1}+#{num2}=#{num1+num2}"
+  puts "#{num1}-#{num2}=#{num1-num2}"
+  puts "#{num1}*#{num2}=#{num1*num2}"
+  puts "#{num1}/#{num2}=#{num1/num2}"
+  i += 1
+
+  if i > steps
+  puts "処理を終了します"
+  break
+  end
+
+end
