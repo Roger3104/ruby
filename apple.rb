2b7abbc1558a6ee7194b9eaa6ae52d3586e36a7f
EC2 Default User <ec2-user@ip-172-31-8-55.ap-northeast-1.compute.internal>
2023-01-09 08:00:12 +0000

    [Finish] 2-5

---
+++ apple.rb
@@ -0,0 +1,11 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+
+elsif apple == "Nagano"
+  puts "この林檎は青森ではなく長野県産です。"
+else 
+  puts "この林檎は青森でも長野県産でもありません。"
+end
+  
---
+++ lesson5.rb
@@ -0,0 +1,12 @@
+total_price = 105
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+  
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+  
+else 
+  puts "みかんを購入することができません。"
+
+end
